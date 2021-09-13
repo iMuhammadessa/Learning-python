e85ecda71342dda68db27809de1df3e746e057d7
Muhammad Essa <imuhammadessa@gmail.com>
2021-09-13 19:30:55 +0500

    File modified

---
+++ my_app.py
@@ -0,0 +1,73 @@
+# Data types
+# name = 'Muhammad'
+# age = 24
+# pi = 3.14
+# cars = ['bmw', 'mercedes', 'range rover']
+
+# print(name)
+# print(age)
+# print(pi)
+# print(cars)
+
+
+# Strings
+# first_name = 'muhammad'
+# surname = 'Essa'
+
+# full_name = first_name.capitalize() + ' ' + surname
+
+# print(full_name)
+# print(len(first_name))
+# print(len(surname))
+# print(full_name.endswith('Essa'))
+
+
+# Numbers + - / * %
+# addition = 10 + 5
+# subtraction = 10 - 5
+# multiplication = 10 * 5
+# division = 10 / 5
+# modulus = 10 % 5
+
+# print(addition)
+# print(subtraction)
+# print(multiplication)
+# print(division)
+# print(modulus)
+
+
+# Boolean Data types True & False
+# print(10 <= 10)
+# print(0 == 1)
+# print(18 > 10)
+# print('Muhammad'.endswith('a'))
+# print('Muhammad'.endswith('d'))
+
+
+# If statements
+# is_adult = True
+# age = 17
+
+# # if is_adult:
+# #     print('is adult')
+
+# if age >= 18:
+#     print('adult')
+# else:
+#     print('not an adult')
+
+
+# Data types list
+# cars = ['bmw', 'tesla', 'mercedes', 'toyota', 'honda']
+# # print(len(cars))
+# # print(cars)
+# # print(cars[0])
+# # print(cars[1])
+# # print(cars[2])
+# # print(cars[3])
+
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#          print(car.capitalize())
